8d0e3e570c450410bc30a7af491bfc29cc6fb506
Guillermo Narvaez-Paliza <gunarvae@tibco.com>
2019-09-30 13:37:57 -0400

    need absolute paths to script files

--- lib/test.sh
+++ lib/test.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-sh update-repos.sh
+sh /home/community-license-check/lib/update-repos.sh
 ruby /home/community-license-check/test/test_community_license_check.rb /home/tibco-streaming-community
-sh license-check-pom.sh
+sh /home/community-license-check/lib/license-check-pom.sh
