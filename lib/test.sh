3bdf84a7b26fb1f7ecb83e7d4f2959d802d81e5f
Guillermo Narvaez-Paliza <gunarvae@tibco.com>
2019-09-30 14:01:52 -0400

    switching to using bash

--- lib/test.sh
+++ lib/test.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 bash /home/community-license-check/lib/update-repos.sh
 ruby /home/community-license-check/test/test_community_license_check.rb /home/tibco-streaming-community
